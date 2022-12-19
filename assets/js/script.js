2137ac3d9b69828df13989e87b3ea7966914249c
romain <romain.wybo1@gmail.com>
2022-12-19 20:55:25 +0100

    starting code

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+VanillaTilt.init(document.querySelectorAll(".card1"), {
+    max: 25,
+    speed: 250,
+  });
+  VanillaTilt.init(document.querySelectorAll(".card2"), {
+    max: 25,
+    speed: 250,
+  });
+  VanillaTilt.init(document.querySelectorAll(".card3"), {
+    max: 25,
+    speed: 250,
+  });
+  VanillaTilt.init(document.querySelectorAll(".card4"), {
+    max: 25,
+    speed: 250,
+  });
