1bc0b741e94952cf91e2513e247191c0f6fc20ab
romain <romain.wybo1@gmail.com>
2022-12-23 20:21:59 +0100

    encore sa fait bcp la non

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,20 @@ const interval = setInterval (()=> {
     clearInterval(interval)
   }
 }, 150)
+
+
+// const formation = document.querySelector(".myself")
+// const txt = " Bonjour, moi c'est Romain. Actuellement étudiant à La Manu, je suis quelqu'un d'appliqué et de sérieux. <br> J'aime dessiner, jouer aux jeux vidéos etcoder. Je porte un intérêt plus particulier au web-design."
+
+// function typewriter(word, index){
+//   if(index < word.length) {
+//     setTimeout(() => {
+//       formation.innerHTML += `<span>${word[index]}</span>`
+//       typewriter(txt, index + 1)
+//     }, 300);
+//   }
+// }
+
+// setTimeout(() => {
+//   typewriter(txt, 0)
+// }, 500)
